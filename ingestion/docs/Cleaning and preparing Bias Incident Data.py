752c54c3829968c85f57a2e54335e17f5479abf4
Jacob Allen Dawson <jacobdawson093@gmail.com>
2026-04-26 16:34:15 -0400

    Update Cleaning and preparing Bias Incident Data.py
    Added category to separate racial vs religious bias

--- ingestion/docs/Cleaning and preparing Bias Incident Data.py
+++ ingestion/docs/Cleaning and preparing Bias Incident Data.py
@@ -13,3 +13,17 @@ df2['suspects_55_years_old'] = pd.to_numeric(df2['suspects_55_years_old'], error
 
 # Create a binary variable for incidents with suspects under 18
 df2['under_18_suspect'] = df2['suspects_less_than_18_years'].apply(lambda x: 1 if x > 0 else 0)
+
+#Create bias category to seperate religious bias to racial/ethnic biases
+def categorize_bias(bias_code):
+    religious_biases = ['Anti-Jewish', 'Anti-Muslim', 'Anti-Catholic', 'Anti-Sikh', 'Anti-Hindhu', 'Anti-Protestant', 'Anti-Buddhist']
+    racial_ethnic_biases = ['Anti-Black', 'Anti-White', 'Anti-Asian', 'Anti-Hispanic', 'Anti-Multi-Racial', 'Anti-American Indian', 'Anti-Other Race/Ethnicity']
+
+    if bias_code in religious_biases:
+        return 'Religious Bias'
+    elif bias_code in racial_ethnic_biases:
+        return 'Racial/Ethnic Bias'
+    else:
+        return 'Other Bias' # For categories not explicitly religious or racial/ethnic
+
+df2['bias_category'] = df2['bias_code'].apply(categorize_bias)
